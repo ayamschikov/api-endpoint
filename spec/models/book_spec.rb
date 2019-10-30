01e338aab6d4532131c7c17cdc21f46f250cb1bc
Aleksandr Yamschikov <aleksandr.yamschikov@rt.ru>
2019-10-31 00:24:30 +0400

    Add book test. refs #2

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @book = create(:book)
+  end
+
+  it 'creates new book' do
+    expect(@book.persisted?).to eq(true)
+  end
+  it "is valid with valid attributes" do
+    expect(@book).to be_valid
+  end
+
+  it "is not valid without a title" do 
+    book = build(:book, title: nil)
+    expect(book).to_not be_valid
+  end
+
+  it "is not valid without a copies_in_stock" do 
+    book = build(:book, copies_in_stock: nil)
+    expect(book).to_not be_valid
+  end
 end
