23b9dfc4d59b07fcc549868623a68aa0596f4449
Aleksandr Yamschikov <aleksandr.yamschikov@rt.ru>
2019-10-31 11:22:23 +0400

    Add order by sold books for publisher api

--- app/serializers/publisher_show_serializer.rb
+++ app/serializers/publisher_show_serializer.rb
@@ -1,3 +1,7 @@
 class PublisherShowSerializer < ActiveModel::Serializer
   has_many :shops, serializer: ShopSerializer, key: :shops
+
+  def shops
+    object.shops.order(books_sold_count: :desc)
+  end
 end
--- spec/controllers/api/publishers_controller_spec.rb
+++ spec/controllers/api/publishers_controller_spec.rb
@@ -7,18 +7,28 @@ RSpec.describe Api::PublishersController, type: :controller do
     @book = create :book, publisher: @publisher
     @shop = create :shop, books_sold_count: 10
     @books_shop = create :books_shop, book: @book, shop: @shop, copies_in_stock: 3
-    shop2 = create :shop
-    create :books_shop, book: @book, shop: shop2
   end
 
   it 'shows shops with books' do
     get :show, params: { id: @publisher.id }
 
     expect(response.status).to eq(200)
-    expect(json[:publisher][:shops].count).to eq(2)
+    expect(json[:publisher][:shops].count).to eq(1)
     expect(json[:publisher][:shops].first[:books_sold_count]).to eq(@shop.books_sold_count)
     expect(json[:publisher][:shops].first[:books_in_stock].first[:id]).to eq(@book.id)
     expect(json[:publisher][:shops].first[:books_in_stock].first[:copies_in_stock]).to eq(@books_shop.copies_in_stock)
+  end
+
+  it 'shows shops ordered by sold books count' do
+    shop2 = create :shop, books_sold_count: 30
+    create :books_shop, book: @book, shop: shop2
+
+    get :show, params: { id: @publisher.id }
+
+    expect(response.status).to eq(200)
+    expect(json[:publisher][:shops].count).to eq(2)
+    expect(json[:publisher][:shops].first[:id]).to eq(shop2.id)
+    expect(json[:publisher][:shops].last[:id]).to eq(@shop.id)
   end
 
   private
