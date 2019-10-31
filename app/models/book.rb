80c6c88a54041f9de553d6b6308707bfa5401f51
Aleksandr Yamschikov <aleksandr.yamschikov@rt.ru>
2019-10-31 08:23:38 +0400

    Add associations in models.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :publisher
+  has_many :books_shops, dependent: :nullify
+  has_many :shops, through: :books_shops
   validates :title, :copies_in_stock, presence: true
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,6 @@
 class Shop < ApplicationRecord
+  has_many :books_shops, dependent: :nullify
+  has_many :books, through: :books_shops
+
   validates :name, presence: true
 end
